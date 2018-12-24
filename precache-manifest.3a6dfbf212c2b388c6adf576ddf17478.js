3586ef6beb0fcc84ada673d04985044e884f61d3
David Applesauce <dsweck@gmail.com>
2018-12-23 22:00:32 -0500

    Updates

---
+++ precache-manifest.3a6dfbf212c2b388c6adf576ddf17478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f880cf94ef251dbec505",
+    "url": "/MemoryGame/static/js/runtime~main.f880cf94.js"
+  },
+  {
+    "revision": "b390e2a6a6e290538d67",
+    "url": "/MemoryGame/static/js/main.b390e2a6.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/MemoryGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "b390e2a6a6e290538d67",
+    "url": "/MemoryGame/static/css/main.06122eab.chunk.css"
+  },
+  {
+    "revision": "6ce14a56b252b7c2c97cb4b8243910ba",
+    "url": "/MemoryGame/index.html"
+  }
+];
